b8f768dbb51ddd18a7607d3b4f973f2de6b7aa8d
Piotr Surma <piotr.smma@gmail.com>
2021-11-01 13:00:52 +0100

    Add Swaggers2 and Swagers

--- src/main/java/pl/piotr/skBackdend/SkBackendApplication.java
+++ src/main/java/pl/piotr/skBackdend/SkBackendApplication.java
@@ -2,8 +2,10 @@ package pl.piotr.skBackdend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SkBackendApplication {
 
 	public static void main(String[] args) {
